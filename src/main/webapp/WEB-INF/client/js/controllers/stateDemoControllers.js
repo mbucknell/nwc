14a76d14040ad297b0c7f8e3183e22b7bea85a81
Carl Schroedl <cschroedl@usgs.gov>
2013-12-11 09:59:36 -0600

    glued huc selection and click disambiguation together.

--- src/main/webapp/WEB-INF/client/js/controllers/stateDemoControllers.js
+++ src/main/webapp/WEB-INF/client/js/controllers/stateDemoControllers.js
@@ -56,9 +56,129 @@ stateDemoControllers.controller('SelectHuc', ['$scope', 'StoredState', 'CommonSt
             name: 'HUC Selection',
             description: 'Find your Hydrologic Unit of interest.'
         },
-        function ($scope, StoredState) {
+        function ($scope, StoredState, CommonState) {
+            $scope.StoredState = StoredState;
+            $scope.CommonState = CommonState;
             
-            console.dir(StoredState);
+            var config = {};
+            var mapLayers = [];
+            var WGS84_GOOGLE_MERCATOR = new OpenLayers.Projection("EPSG:900913");
+             var EPSG900913Options = {
+                sphericalMercator: true,
+                layers: "0",
+                isBaseLayer: true,
+                projection: WGS84_GOOGLE_MERCATOR,
+                units: "m",
+                buffer: 3,
+                transitionEffect: 'resize',
+                wrapDateLine: false
+            };
+             // ////////////////////////////////////////////// BASE LAYERS
+            var zyx = '/MapServer/tile/${z}/${y}/${x}';
+            mapLayers.push(new OpenLayers.Layer.XYZ(
+                    "World Light Gray Base",
+                    "http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base" + zyx,
+                    Object.merge(EPSG900913Options, {numZoomLevels: 14})
+                    ));
+            mapLayers.push(new OpenLayers.Layer.XYZ(
+                    "World Topo Map",
+                    "http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map" + zyx,
+                    {isBaseLayer: true, units: "m"}));
+            mapLayers.push(new OpenLayers.Layer.XYZ(
+                    "World Imagery",
+                    "http://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery" + zyx,
+                    {isBaseLayer: true, units: "m"}));
+            mapLayers.push(new OpenLayers.Layer.XYZ(
+                    "World Street Map",
+                    "http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map" + zyx,
+                    {isBaseLayer: true, units: "m"}));
+            mapLayers.push(new OpenLayers.Layer.XYZ(
+                    "World Terrain Base",
+                    "http://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief" + zyx,
+                    Object.merge(EPSG900913Options, {numZoomLevels: 14})
+                    ));
+            var workflowLayerOptions = {
+                        opacity: 0.8,
+                        displayInLayerSwitcher: false,
+                        visibility: true,
+                        isBaseLayer : false
+                    };
+
+            var hucLayer = new OpenLayers.Layer.WMS("National WBD Snapshot",
+                    'http://cida-wiwsc-wsdev.er.usgs.gov:8081/geoserver/' + 'gwc/service/wms',
+                    {
+                            layers: 'NHDPlusHUCs:NationalWBDSnapshot',
+                            transparent: true,
+                            styles: ['polygon']
+                    },
+                    workflowLayerOptions
+            );
+            mapLayers.push(hucLayer);
+            var controls = [
+                    new OpenLayers.Control.Navigation(),
+                    new OpenLayers.Control.MousePosition({
+                        prefix: 'POS: '
+                    }),
+                    new OpenLayers.Control.ScaleLine({
+                        geodesic: true
+                    }),
+                    new OpenLayers.Control.LayerSwitcher({
+                        roundedCorner: true
+                    }),
+                    new OpenLayers.Control.Zoom()
+            ];
+            var extent = new OpenLayers.Bounds(-146.0698, 19.1647, -42.9301, 52.8949).transform(new OpenLayers.Projection("EPSG:4326"), new OpenLayers.Projection("EPSG:900913"));
+
+            var map = new OpenLayers.Map('hucSelectMap', {
+                layers: mapLayers,
+                restrictedExtent: extent,
+                projection: WGS84_GOOGLE_MERCATOR,
+                controls: controls
+            });
+            var hucsGetFeatureInfoControl = new OpenLayers.Control.WMSGetFeatureInfo({
+                title: 'huc-identify-control',
+                hover: false,
+                layers: [
+                    hucLayer
+                ],
+                queryVisible: true,
+                output: 'object',
+                drillDown: true,
+                infoFormat: 'application/vnd.ogc.gml',
+                vendorParams: {
+                    radius: 5
+                },
+                id: 'hucs',
+                autoActivate: true
+            });
+            var featureInfoHandler = function(responseObject){
+            //for some reason the real features are inside an array
+                var actualFeatures = responseObject.features[0].features;
+                var hucCount = actualFeatures.length;
+                if(0 === hucCount){
+                    //nothing
+                }
+                else if(1 === hucCount){
+                    StoredState.hucId = actualFeatures[0].attributes.HUC_12;
+                    $('#goToFinalStep').click();
+                }
+                else{
+                    CommonState.ambiguousHucs = actualFeatures;
+                    $('#goToDisabiguateClick').click();
+                }
+
+
+            };
+            hucsGetFeatureInfoControl.events.register("getfeatureinfo", {}, featureInfoHandler);
+            map.addControl(hucsGetFeatureInfoControl);
+
+            map.zoomToExtent(extent, true);
+        
+        
+        
+            
+            
+            console.dir(CommonState);
         }
     )
 ]);
@@ -70,26 +190,7 @@ stateDemoControllers.controller('DisambiguateClick', ['$scope', 'StoredState', '
             description: 'Your click fell near multiple HUCs. Select one from the list to continue.'
         },
         function ($scope, StoredState, CommonState) {
-            //<mocking huc objects>
-            CommonState.ambiguousHucs = [
-                {
-                    id: 42,
-                    name: 'Best HUC',
-                    area: 423556
-                },
-                {
-                    id: 2,
-                    name: 'Better HUC',
-                    area: 496
-                },
-                {
-                    id: 4,
-                    name: 'My very own long-named HUC',
-                    area: 1.9878
-                }
-            ];
-            //</mocking huc objects>
-            
+             
             $scope.hucs = CommonState.ambiguousHucs;
             
             //your list of huc objects is in CommonState.ambiguousHucs
@@ -97,7 +198,7 @@ stateDemoControllers.controller('DisambiguateClick', ['$scope', 'StoredState', '
             //@todo bind CommonState.ambiguousHucs and scope
             
             //@todo bind StoredState.hucId to scope's hucId            
-            console.dir(StoredState);
+
         }
     )
 ]);
