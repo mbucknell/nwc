8bcf3d0ded181c32ebcef6a4549c89d0c6c7459a
Carl Schroedl <cschroedl@usgs.gov>
2014-01-13 16:39:15 -0600

    Added function to dynamically load stylesheets.

--- src/main/webapp/WEB-INF/includes/js/global.js
+++ src/main/webapp/WEB-INF/includes/js/global.js
@@ -1,1 +1,10 @@
-
+if(!NWC){
+    var NWC = {};
+}
+/**
+  * function to load a given css file 
+  */ 
+ NWC.loadCSS = function(href) {
+     var cssLink = $("<link rel='stylesheet' type='text/css' href='"+href+"'>");
+     $("head").append(cssLink); 
+ };
