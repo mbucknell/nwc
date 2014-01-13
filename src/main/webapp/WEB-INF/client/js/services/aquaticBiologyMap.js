4ba3c6ecc687247d7fbc9196bdd4d1e8e7ec7e33
Carl Schroedl <cschroedl@usgs.gov>
2014-01-13 16:40:09 -0600

    Added aquatic bio map

--- src/main/webapp/WEB-INF/client/js/services/aquaticBiologyMap.js
+++ src/main/webapp/WEB-INF/client/js/services/aquaticBiologyMap.js
@@ -1,36 +1,64 @@
 /*global angular,OpenLayers,CONFIG*/
 (function () {
-    var waterBudgetMap = angular.module('nwc.map.aquaticBiology', []);
-    waterBudgetMap.factory('AquaticBiologyMap', [ 'StoredState', 'CommonState', '$state', 'BaseMap',
-       function(StoredState, CommonState, $state, BaseMap){
-           var privateMap; 
-    
-        var initMap = function () {
-            var mapLayers = [];
-            var controls = [];
-           
+    var aquaticBiologyMap = angular.module('nwc.map.aquaticBiology', []);
+    aquaticBiologyMap.factory('AquaticBiologyMap', ['StoredState', 'CommonState', '$state', 'BaseMap', '$log',
+        function (StoredState, CommonState, $state, BaseMap, $log) {
+            var privateMap;
+
+            var initMap = function () {
+                var mapLayers = [];
+                var initialControls = [];
+                var bioDataSitesLayer = new OpenLayers.Layer.WMS(
+                        "BioData Sites",
+                        CONFIG.endpoint.geoserver + 'wms',
+                        {
+                            layers: 'BioData:SiteInfo',
+                            transparent: true
+                        },
+                        BaseMap.getWorkflowLayerOptions()
+                        );
+                bioDataSitesLayer.id = 'biodata-sites-feature-layer';
+                mapLayers.push(bioDataSitesLayer);
+                
+                var waterCensusToolbar = new OpenLayers.Control.WaterCensusToolbar({}, new OpenLayers.Control.BioSitesSelectionTool());
+                initialControls.push(waterCensusToolbar);
+                                var bioDataGetFeatureControl = new OpenLayers.Control.GetFeature({
+                    protocol: new OpenLayers.Protocol.WFS({
+                        version: "1.1.0",
+                        url: CONFIG.endpoint.geoserver + 'wfs',
+                        featureType: 'SiteInfo',
+                        featureNS: 'gov.usgs.biodata.aquatic',
+                        srsName: 'EPSG:900913'
+                    }),
+                    box: true,
+                    id: 'bioDataSites'
+                });
+                initialControls.push(bioDataGetFeatureControl);
+                bioDataGetFeatureControl.events.register('featuresselected', {}, function (e) {
+                    CommonState.aquaticBiologySites = e.features;
+                    $log.info(CommonState);
+                });
                 
                 var map = BaseMap.new({
                     layers: mapLayers,
-                    controls: controls
+                    controls: initialControls
                 });
-            
-            
-            //stash it in a closure var
-            privateMap = map;
-            return privateMap;
-        };
-        var getMap = function () {
-            if (!privateMap) {
-                initMap();
-            }
-            return privateMap;
-        };
-        return {
-            initMap: initMap,
-            getMap: getMap
-        };
-       } 
+
+                //stash it in a closure var
+                privateMap = map;
+                return privateMap;
+            };
+            var getMap = function () {
+                if (!privateMap) {
+                    initMap();
+                }
+                return privateMap;
+            };
+            return {
+                initMap: initMap,
+                getMap: getMap
+            };
+        }
     ]);
-    
+
 }());
