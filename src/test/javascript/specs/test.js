58ef740d0137653c9d2ef3d98b7e67794e4095dd
Carl Schroedl <cschroedl@usgs.gov>
2014-02-03 14:54:09 -0600

    Added proof-of-concept test

--- src/test/javascript/specs/test.js
+++ src/test/javascript/specs/test.js
@@ -1,5 +1,9 @@
-describe('something', function(){
-	it('should do things', function(){
-		expect(true).toBe(false);
-	});
+//trivial test, just for show:
+describe('Conversion', function(){
+        var $injector = angular.injector(['nwc.conversion']);
+        var Convert = $injector.get('Convert');
+        it('should convert square miles to acres', function(){
+            expect(Convert.squareMilesToAcres(1)).toBe(640.0);
+        });
+
 });
