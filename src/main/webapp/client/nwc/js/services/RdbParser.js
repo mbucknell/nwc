3e8fa89bc8cea6e4c3de5294f5b84a03f2580542
Carl Schroedl <cschroedl@usgs.gov>
2014-02-17 15:25:27 -0600

    Testing out parser, RdbTable functionality.

--- src/main/webapp/client/nwc/js/services/RdbParser.js
+++ src/main/webapp/client/nwc/js/services/RdbParser.js
@@ -1,13 +1,14 @@
 /*global angular*/
 (function () {
-    var rdbParser = angular.module('nwc.rdpParser', []);
+    var rdbParser = angular.module('nwc.rdbParser', []);
     var RdbTable = function(){
         var self = this === window ? {} : this;
         //a map of column name to row number
-        self.columnHeaders = {}; 
+        self.columnHeaders = []; 
         self.data = [];//row-oriented
         self.getColumnByIndex = function(index){
-            return subsetTableByColumnIndices([index]);
+            var columns = self.subsetTableByColumnIndices([index]);
+            return columns[0];
         };
         self.subsetRow = function(row, rowIndices){
             var subsettedRow = rowIndices.map(
@@ -18,19 +19,107 @@
             return subsettedRow;
         };
         self.subsetTableByColumnIndices= function(indices){
-            var subsettedTable = this.data.map(function(row){
-               return subsetRow(row, indices); 
+            var subsettedTable = [];
+            (indices.length).times(function(){
+                subsettedTable.push([]);
+            });
+            self.data.each(function(row){
+               var columns = self.subsetRow(row, indices);
+               columns.each(function(value, key){
+                   subsettedTable[key].push(value);
+               });
             });
             return subsettedTable;
         };
         self.getIndexOfColumnName = function(columnName){
-            return self.columnHeaders[columnName];
+            return self.columnHeaders.indexOf(columnName);
         };
         self.getColumnByName = function(columnName){
             var columnIndex = self.getIndexOfColumnName(columnName);
+            var column = self.getColumnByIndex(columnIndex);
+            return column;
         };
         
     };
+    rdbParser.service('rdbTable', [
+       function(){
+           return {
+               new: function(){
+                   return new RdbTable();
+               }
+           };
+       }
+    ]);
+    var RowType= {
+        COMMENTS : 0,
+        COLUMN_NAMES: 1,
+        COLUMN_DEFINITION: 2,
+        DATA: 3
+    };
+    
+    var commentPrefix = '#';
+    var lineDelimeter = '\n';
+    var columnDelimeter = '\t';
+    var isComment = function(line){
+        return commentPrefix === line[0];
+    };
+    rdbParser.service('rdbParser', ['rdbTable',
+        function (rdbTable) {
+            return {
+                /**
+                 * Given an rdb csv string, returns an array of parsed RdbTable objects.
+                 * The parser returns an array because a single rdb csv string may contain multiple tables.
+                 * @param {String} text an rdb-style csv
+                 * @returns {Array<RdbTable>}
+                 */
+                parse: function (text) {
+                    var state = RowType.COMMENTS;
+                    var lines = text.split(lineDelimeter);
+
+                    var tables = [];
+                    var currentTable = new RdbTable();
+
+                    angular.forEach(lines, function (line, lineIndex) {
+                        if (line.length) {
+                            if (isComment(line)) {
+                                if (state === RowType.DATA) {
+                                    tables.push(currentTable);
+                                    currentTable = new RdbTable();
+                                }
+                                state = RowType.COMMENTS;
+                            }
+                            else {
+                                if (state === RowType.COMMENTS) {
+                                    state = RowType.COLUMN_NAMES;
+                                    var columnNames = line.split(columnDelimeter);
+                                    currentTable.columnHeaders = columnNames;
+                                }
+                                else if (state === RowType.COLUMN_NAMES) {
+                                    state = RowType.COLUMN_DEFINITION;
+                                    //nothing else
+                                }
+                                else if (state === RowType.COLUMN_DEFINITION) {
+                                    state = RowType.DATA;
+                                    var data = line.split(columnDelimeter);
+                                    currentTable.data.push(data);
+                                }
+                                else if (state === RowType.DATA) {
+                                    var data = line.split(columnDelimeter);
+                                    currentTable.data.push(data);
+                                }
+                                else {
+                                    throw Error('Undefined Parser State');
+                                }
+                            }
+                        }
+                    });
+                    tables.push(currentTable);
+                    return tables;
+                    
+                }
+            };
+        }
+    ]);
     
     
 }());
