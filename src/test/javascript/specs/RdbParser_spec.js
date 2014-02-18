801a870163d5df7cc68110cd64f983b58a99ad60
Carl Schroedl <cschroedl@usgs.gov>
2014-02-18 09:58:17 -0600

    Reformat FOR REAL.

--- src/test/javascript/specs/RdbParser_spec.js
+++ src/test/javascript/specs/RdbParser_spec.js
@@ -1,59 +1,59 @@
 describe('RdbParser', function () {
     var firstTableHeaders = ['agency_cd', 'site_no', 'datetime', '02_00060_00003', '02_00060_00003_cd'];
-        var secondTableHeaders = ['agency_cd', 'site_no', 'datetime', 'blah', 'blah'];
-        var makeHeaderLine = function (headerArray) {
-            return headerArray.join('\t') + '\n';
-        };
-        var firstTableRowCount = 5;
-        var secondTableRowCount = 2;
-        var rdbSample = '# ---------------------------------- WARNING ----------------------------------------\n'
-                + '# Provisional data are subject to revision. Go to\n'
-                + '# http://waterdata.usgs.gov/nwis/help/?provisional for more information.\n'
-                + '#\n'
-                + '# File-format description:  http://waterdata.usgs.gov/nwis/?tab_delimited_format_info\n'
-                + '# Automated-retrieval info: http://waterdata.usgs.gov/nwis/?automated_retrieval_info\n'
-                + '#\n'
-                + '# Contact:   gs-w_support_nwisweb@usgs.gov\n'
-                + '# retrieved: 2014-02-17 14:32:53 EST	(natwebcaas01)\n'
-                + '#\n'
-                + '# Data for the following 2 site(s) are contained in this file\n'
-                + '#    USGS 01480000 RED CLAY CREEK AT WOODDALE, DE\n'
-                + '#    USGS 01481500 BRANDYWINE CREEK AT WILMINGTON, DE\n'
-                + '# -----------------------------------------------------------------------------------\n'
-                + '#\n'
-                + '# Data provided for site 01480000\n'
-                + '#    DD parameter statistic   Description\n'
-                + '#    02   00060     00003     Discharge, cubic feet per second (Mean)\n'
-                + '#\n'
-                + '# Data-value qualification codes included in this output:\n'
-                + '#     A  Approved for publication -- Processing and review completed.\n'
-                + '#     e  Value has been edited or estimated by USGS personnel and is write protected.\n'
-                + '#     P  Provisional data subject to revision.\n'
-                + '#\n'
-                + makeHeaderLine(firstTableHeaders)
-                + '5s	15s	20d	14n	10s\n'
-                + 'USGS	01480000	1990-01-01	210	A:e\n'
-                + 'USGS	01480000	1990-01-02	84	A\n'
-                + 'USGS	01480000	1990-01-04	58	A\n'
-                + 'USGS	01480000	1990-01-05	61	A\n'
-                + 'USGS	01480000	2014-02-16	93	P\n'
-                + '# Data provided for site 01481500\n'
-                + '#    DD parameter statistic   Description\n'
-                + '#    02   00060     00003     Discharge, cubic feet per second (Mean)\n'
-                + '#\n'
-                + '# Data-value qualification codes included in this output:\n'
-                + '#     A  Approved for publication -- Processing and review completed.\n'
-                + '#     e  Value has been edited or estimated by USGS personnel and is write protected.\n'
-                + '#     P  Provisional data subject to revision.\n'
-                + '#\n'
-                + makeHeaderLine(secondTableHeaders)
-                + '5s	15s	20d	14n	10s\n'
-                + 'USGS	01481500	1990-01-01	1070	A:e\n'
-                + 'USGS	01481500	1990-01-02	799	A\n';
+    var secondTableHeaders = ['agency_cd', 'site_no', 'datetime', 'blah', 'blah'];
+    var makeHeaderLine = function (headerArray) {
+        return headerArray.join('\t') + '\n';
+    };
+    var firstTableRowCount = 5;
+    var secondTableRowCount = 2;
+    var rdbSample = '# ---------------------------------- WARNING ----------------------------------------\n'
+            + '# Provisional data are subject to revision. Go to\n'
+            + '# http://waterdata.usgs.gov/nwis/help/?provisional for more information.\n'
+            + '#\n'
+            + '# File-format description:  http://waterdata.usgs.gov/nwis/?tab_delimited_format_info\n'
+            + '# Automated-retrieval info: http://waterdata.usgs.gov/nwis/?automated_retrieval_info\n'
+            + '#\n'
+            + '# Contact:   gs-w_support_nwisweb@usgs.gov\n'
+            + '# retrieved: 2014-02-17 14:32:53 EST	(natwebcaas01)\n'
+            + '#\n'
+            + '# Data for the following 2 site(s) are contained in this file\n'
+            + '#    USGS 01480000 RED CLAY CREEK AT WOODDALE, DE\n'
+            + '#    USGS 01481500 BRANDYWINE CREEK AT WILMINGTON, DE\n'
+            + '# -----------------------------------------------------------------------------------\n'
+            + '#\n'
+            + '# Data provided for site 01480000\n'
+            + '#    DD parameter statistic   Description\n'
+            + '#    02   00060     00003     Discharge, cubic feet per second (Mean)\n'
+            + '#\n'
+            + '# Data-value qualification codes included in this output:\n'
+            + '#     A  Approved for publication -- Processing and review completed.\n'
+            + '#     e  Value has been edited or estimated by USGS personnel and is write protected.\n'
+            + '#     P  Provisional data subject to revision.\n'
+            + '#\n'
+            + makeHeaderLine(firstTableHeaders)
+            + '5s	15s	20d	14n	10s\n'
+            + 'USGS	01480000	1990-01-01	210	A:e\n'
+            + 'USGS	01480000	1990-01-02	84	A\n'
+            + 'USGS	01480000	1990-01-04	58	A\n'
+            + 'USGS	01480000	1990-01-05	61	A\n'
+            + 'USGS	01480000	2014-02-16	93	P\n'
+            + '# Data provided for site 01481500\n'
+            + '#    DD parameter statistic   Description\n'
+            + '#    02   00060     00003     Discharge, cubic feet per second (Mean)\n'
+            + '#\n'
+            + '# Data-value qualification codes included in this output:\n'
+            + '#     A  Approved for publication -- Processing and review completed.\n'
+            + '#     e  Value has been edited or estimated by USGS personnel and is write protected.\n'
+            + '#     P  Provisional data subject to revision.\n'
+            + '#\n'
+            + makeHeaderLine(secondTableHeaders)
+            + '5s	15s	20d	14n	10s\n'
+            + 'USGS	01481500	1990-01-01	1070	A:e\n'
+            + 'USGS	01481500	1990-01-02	799	A\n';
 
-        var $injector = angular.injector(['nwc.rdbParser']);
-        var parser = $injector.get('rdbParser');
-        var rdbTables = parser.parse(rdbSample);
+    var $injector = angular.injector(['nwc.rdbParser']);
+    var parser = $injector.get('rdbParser');
+    var rdbTables = parser.parse(rdbSample);
 
     describe('RdbParser.parse', function () {
         it('should parse the correct number of headers', function () {
