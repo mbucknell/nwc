51ca7e878541613f8d619dec688ec5f369021feb
Carl Schroedl <cschroedl@usgs.gov>
2014-02-25 13:53:56 -0600

    Broken commit - customizing serialization of StoredState's circular-referencing objects

--- src/main/webapp/client/nwc/state/sharedStateServices.js
+++ src/main/webapp/client/nwc/state/sharedStateServices.js
@@ -12,7 +12,6 @@ var storedState = Object.extended({
 var commonState = Object.extended({
     DataSeriesStore: Object.extended(),
     newDataSeriesStore: false
-    
 });
 
 //this factory provides access to the state that is NOT stored to the server, but that
@@ -40,7 +39,18 @@ sharedStateServices.factory('StatePersistence', [
             var restore = function (stateId) {
                 $http.get('../../loadsession/' + stateId)
                     .success(function (data) {
-                        Object.merge(StoredState, data);
+                        
+                        var customDeserializationProperties = Object.keys(customDeserializers);
+                        var customlyDeserializedState = Object.reject(data, customDeserializationProperties);
+
+                        customDeserializationProperties.each(function (customDeserializationProperty) {
+                            var customDeserializer = customDeserializers[customDeserializationProperty];
+                            var valueToDeserialize = data[customDeserializationProperty];
+                            var customValue = customDeserializer(valueToDeserialize);
+                            customlyDeserializedState[customDeserializationProperty] = customValue;
+                        });
+                        
+                        Object.merge(StoredState, customlyDeserializedState);
                         //let listeners on StoredState run before changing state
                         $timeout(function(){
                             $state.go(StoredState.stateName, StoredState.stateParams);
@@ -52,9 +62,42 @@ sharedStateServices.factory('StatePersistence', [
                         });
                     });
             };
+            
+            
+            var geoJsonFormatter = new OpenLayers.Format.GeoJson();
+            
+            /**
+             * A few objects cannot use the default JSON.stringify serialization because they
+             * contain unserializable circular references. These functions catch
+             * those objects and customize their serializations.
+             */
+            //map of property name to custom serialization function
+            var customSerializers = {
+                'hucFeature' : function(hucFeature){
+                    var serializedHuc = geoJsonFormatter.write(hucFeature);
+                    return serializedHuc;
+                }
+            };
+            //map of property name to custom deserialization function
+            var customDeserializers = {
+                'hucFeature': function(hucFeatureString){
+                    var deserializedHuc = geoJsonFormatter.read(hucFeatureString);
+                    return deserializedHuc;
+                }
+            };
 
             var store = function (stateObject) {
-                $http.post('../../savesession', stateObject)
+                var customSerializationProperties = Object.keys(customSerializers);
+                var nonCircularState = Object.reject(stateObject, customSerializationProperties);
+                
+                customSerializationProperties.each(function(customSerializationProperty){
+                   var customSerializer = customSerializers[customSerializationProperty];
+                   var circularValue = stateObject[customSerializationProperty];
+                   var customValue = customSerializer(circularValue);
+                   nonCircularState[customSerializationProperty] = customValue;
+                });
+                
+                $http.post('../../savesession', nonCircularState)
                     .success(function (data) {
                         console.dir(data);
                         $modal.open({
