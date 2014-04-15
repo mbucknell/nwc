4020a93844f43feb6ccd461d5d4b60d5e1045057
Jordan Walker <jiwalker@usgs.gov>
2014-04-15 12:12:14 -0500

    water year utility should be able to be hooked up to select

--- src/main/webapp/client/nwc/general/services/WaterYearUtil.js
+++ src/main/webapp/client/nwc/general/services/WaterYearUtil.js
@@ -3,10 +3,14 @@
     var waterYear = angular.module('nwc.waterYear', []);
     waterYear.service('waterYearUtil', [
         function(){
+            var extractYear = function(date) {
+                return date.format('{yyyy}').toNumber();
+            };
+            
             return {
                 dateToWaterYear: function(date) {
                     var sugarDate = Date.create(date);
-                    var year = sugarDate.format('{yyyy}').toNumber();
+                    var year = extractYear(sugarDate);
                     if (sugarDate.is('October') || sugarDate.is('November') || sugarDate.is('December')) {
                         year = year + 1;
                     }
@@ -17,6 +21,32 @@
                 },
                 waterYearEnd: function(wy) {
                     return Date.create(wy, 8, 30); // months are 0 indexed for some reason
+                },
+                waterYearRange: function(range) {
+                    var wyStart = undefined;
+                    var wyEnd = undefined;
+                    
+                    var oct1 = Date.create(extractYear(range.start), 9, 1); // Oct 1 start year
+                    if (oct1.isAfter(range.start)) {
+                        wyStart = oct1;
+                    } else {
+                        wyStart = oct1.advance('1 year');
+                    }
+                    
+                    var sep30 = Date.create(extractYear(range.end), 8, 30); // Sep 30 end year
+                    if (sep30.isBefore(range.end)) {
+                        wyEnd = sep30;
+                    } else {
+                        wyEnd = sep30.rewind('1 year');
+                    }
+                    return Date.range(wyStart, wyEnd);
+                },
+                yearsAsArray: function(range, descending) {
+                    var years = [];
+                    range.every('year', function(d) {
+                        (descending) ? years.unshift(extractYear(d)) : years.push(extractYear(d));
+                    });
+                    return years;
                 }
             };
         }
--- src/test/javascript/specs/WaterYearUtil_spec.js
+++ src/test/javascript/specs/WaterYearUtil_spec.js
@@ -16,5 +16,34 @@ describe('WaterYear', function () {
         it('should be October 1', function () {
             expect(util.waterYearStart(2000).is("1999/10/01")).toBe(true);
         });
+        it('should be September 30', function() {
+            expect(util.waterYearEnd(2000).is("2000/09/30")).toBe(true);
+        });
+    });
+    describe('WaterYear.waterYearRange', function() {
+        var range = util.waterYearRange(Date.range('1973/01/01', '1995/01/01'));
+        it('should start on October 1, 1973', function() {
+            expect(range.start.is('1973/10/01')).toBe(true);
+        });
+        it('should not contain September 30, 1973', function() {
+            expect(range.contains('1973/09/30')).toBe(false);
+        });
+        it('should not contain October 1, 1994', function() {
+            expect(range.contains('1994/10/01')).toBe(false);
+        });
+        it('should end on September 30, 1994', function() {
+            expect(range.end.is('1994/09/30')).toBe(true);
+        });
+    });
+    describe('WaterYear.yearsAsArray', function() {
+        it('should have 5 years', function() {
+            expect(util.yearsAsArray(util.waterYearRange(Date.range('1973/01/01', '1979/01/01'))).length).toBe(5);
+        });
+        it('reversed should have 1974 as 2nd element', function() {
+            expect(util.yearsAsArray(util.waterYearRange(Date.range('1973/01/01', '1979/01/01')))[1]).toBe(1974);
+        });
+        it('reversed should have 1974 as 4th element', function() {
+            expect(util.yearsAsArray(util.waterYearRange(Date.range('1973/01/01', '1979/01/01')), true)[3]).toBe(1974);
+        });
     });
 });
