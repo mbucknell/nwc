569fb60f1ba1c1b534727c458c47a7390c75d368
dschmitt <dschmitt@usgs.gov>
2014-10-22 12:35:04 -0500

    WaterSmart-404: Resolved conflicts with directive code (pull 154).

---
+++ nwc/src/main/webapp/client/nwc/general/directives/featureMap.js
@@ -0,0 +1,33 @@
+(function() {
+	/*
+	 * Directive is used as an attribute on an element where you would like a simple map with no
+	 * controls, a base layer, and a user defined layer based in from the attribute value, nwc-feature-map.
+	 */
+	var featureMap = angular.module('nwc.directives.featureMap', []);
+
+	featureMap.directive('nwcFeatureMap', function() {
+		var link = function(scope, element, attrs) {
+			var map = new OpenLayers.Map(element.attr('id'),{controls: [new OpenLayers.Control.Zoom()],'restrictedExtent': scope.bounds()});
+			map.addLayer(scope.layer());
+
+			var baseLayer = new OpenLayers.Layer.XYZ("World Street Map",
+					"http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/${z}/${y}/${x}", {
+						isBaseLayer: true,
+						units: "m",
+						sphericalMercator: true
+			});
+			map.addLayer(baseLayer);
+			map.zoomToExtent(map.restrictedExtent);
+		};
+		return {
+			restrict : 'A',
+			link : link,
+			scope : {
+				layer : '&',
+				bounds : '&'
+			}
+		};
+	});
+
+}());
+
