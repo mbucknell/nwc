a9f0fe6a69ba6dfc0a0f5a232fdf84eb03e920e0
Mary Bucknell <mbucknell@usgs.gov>
2015-03-02 11:49:45 -0600

    Added selection controls to the streamflow-stats page. Note that the old application was using WFSGetFeature for the huc layer. I'm not sure why other then to get the feature into the next workflow step. I switched to using WMSGetFeatureInfo to make it similar to the others.

--- nwc/src/main/webapp/js/view/StreamflowStatsMapView.js
+++ nwc/src/main/webapp/js/view/StreamflowStatsMapView.js
@@ -53,13 +53,77 @@ NWC.view.StreamflowStatsMapView = NWC.view.BaseSelectMapView.extend({
 			}
 		);
 
-		this.selectControl = new OpenLayers.Control();
+		var gageFeatureInfoHandler = function(responseObject) {
+			if (responseObject.features.length === 0) {
+				// nothing
+			}
+			else {
+				console.log('Selected a feature');
+			}
+		};
+		this.gageControl = new OpenLayers.Control.WMSGetFeatureInfo({
+			title : 'selection-control',
+			hover : false,
+			autoActivate : false,
+			layers : [this.gagesLayer],
+			queryVisible : true,
+			infoFormat: 'application/vnd.ogc.gml',
+			vendorParams: {
+				radius: 5
+			}
+		});
+		this.gageControl.events.register("getfeatureinfo", this, gageFeatureInfoHandler);
+
+		var hucFeatureHandler = function(responseObject) {
+			var sortedFeature;
+			if (responseObject.features.length === 0) {
+				//nothing
+			}
+			else {
+				sortedFeature = responseObject.features.min(function(el) {
+					var result = Infinity;
+
+                    var mi2 = parseFloat(el.data.mi2);
+					if (mi2) {
+						result = mi2;
+					}
+
+					return result;
+				});
+			}
+
+			// Determine if the model for this feature is valid
+			var km2 = NWC.util.Convert.acresToSquareKilometers(
+						NWC.util.Convert.squareMilesToAcres(sortedFeature.data.mi2));
+			if (km2 > 2000) {
+				alert("Hydrologic model results are not valid for watersheds this large (" + km2.round(0) + " km^2), please choose a smaller watershed.");
+			} else {
+				console.log('Go to huc 12 ' + sortedFeature.attributes.HUC12);
+			}
+		};
+
+		this.hucsControl = new OpenLayers.Control.WMSGetFeatureInfo({
+			title : 'huc-control',
+			hover : false,
+			autoActivate : false,
+			layers : [this.hucLayer],
+			queryVisible : true,
+			infoFormat: 'application/vnd.ogc.gml',
+			vendorParams: {
+				radius: 5
+			}
+		});
+		this.hucsControl.events.register('getfeatureinfo', {}, hucFeatureHandler);
+
+		this.selectControl = this.gageControl;
 
 		$.extend(this.events, NWC.view.BaseSelectMapView.prototype.events);
 		NWC.view.BaseSelectMapView.prototype.initialize.apply(this, arguments);
 
 		this.map.addLayers([this.gagesLayer, this.hucLayer]);
 		this.addFlowLines();
+
+		this.map.addControl(this.hucsControl);
 
 		this.listenTo(this.model, 'change:streamflowType', this.updateSelectionLayer);
 		this.updateSelectionLayer();
@@ -69,23 +133,43 @@ NWC.view.StreamflowStatsMapView = NWC.view.BaseSelectMapView.extend({
 		var $modeledInfo = $('#modeled-streamflow-warning-div');
 		var $observedInfo = $('#observed-streamflow-info-div');
 		var streamflowType = this.model.get('streamflowType');
+
 		var gageVisible = streamflowType === 'observed';
 		var hucVisible = streamflowType === 'modeled';
-$
+
+		var setControlActive = function(toControl, active) {
+			if (active) {
+				toControl.activate();
+			}
+			else {
+				toControl.deactivate();
+			}
+		};
+
+		// Set layer visibility
 		this.gagesLayer.setVisibility(gageVisible);
 		this.hucLayer.setVisibility(hucVisible);
+
 		if (hucVisible) {
 			$modeledInfo.show();
+			setControlActive(this.hucsControl, this.selectControl.active);
+			this.gageControl.deactivate();
+			this.selectControl = this.hucsControl;
 		}
 		else {
 			$modeledInfo.hide();
 		}
+
 		if (gageVisible) {
 			$observedInfo.show();
+			setControlActive(this.gageControl, this.selectControl.active);
+			this.hucsControl.deactivate();
+			this.selectControl = this.gageControl;
 		}
 		else {
 			$observedInfo.hide();
 		}
+
 		// Because this shifts the map's location on the page, call updateSize
 		this.map.updateSize();
 	},
