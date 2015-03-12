1a963caeb009345e6ff29278d3967500af0754b5
Mary Bucknell <mbucknell@usgs.gov>
2015-03-12 14:57:35 -0500

    Added the statistics groups and data buttons.

--- nwc/src/main/webapp/js/view/StreamflowStatsHucDataView.js
+++ nwc/src/main/webapp/js/view/StreamflowStatsHucDataView.js
@@ -25,6 +25,38 @@ NWC.view.StreamflowStatsHucDataView = NWC.view.BaseView.extend({
 	initialize : function(options) {
 		this.context.hucId = options.hucId;
 		this.context.years = NWC.util.WaterYearUtil.yearsAsArray(NWC.util.WaterYearUtil.waterYearRange(Date.range(this.MIN_DATE, this.MAX_DATE)));
+		this.context.streamStatsOptions = [
+            {
+                shortName: "magnifSeven",
+                longName: "Seven Fundamental Daily Streamflow Statistics",
+                url: "http://onlinelibrary.wiley.com/doi/10.1002/rra.2710/abstract"
+            },
+            {
+                shortName: "magStat",
+                longName: "Flow Magnitude",
+                url: "http://pubs.er.usgs.gov/publication/ofr20061093"
+            },
+            {
+                shortName: "flowStat",
+                longName: "Flow Frequency",
+                url: "http://pubs.er.usgs.gov/publication/ofr20061093"
+            },
+            {
+                shortName: "durStat",
+                longName: "Flow Duration",
+                url: "http://pubs.er.usgs.gov/publication/ofr20061093"
+            },
+            {
+                shortName: "timStat",
+                longName: "Flow Timing",
+                url: "http://pubs.er.usgs.gov/publication/ofr20061093"
+            },
+            {
+                shortName: "rateStat",
+                longName: "Rate of Change",
+                url: "http://pubs.er.usgs.gov/publication/ofr20061093"
+            }
+        ]
 		this.insetMapDiv = options.insetMapDiv;
 
 		var baseLayer = NWC.util.mapUtils.createWorldStreetMapLayer();
@@ -51,10 +83,14 @@ NWC.view.StreamflowStatsHucDataView = NWC.view.BaseView.extend({
 
 		NWC.view.BaseView.prototype.initialize.apply(this, arguments);
 		this.map.zoomToExtent(this.map.getMaxExtent());
+
+		// Initialize DOM on page
 		var $start = $('#start-year option[value="' + this.context.years.first() + '"]');
 		var $end = $('#end-year option[value="' + this.context.years.last() + '"]');
 		$start.prop('selected', true);
 		$end.prop('selected', true);
+
+		$('#calculate-stats-button').d
 	}
 
 });
