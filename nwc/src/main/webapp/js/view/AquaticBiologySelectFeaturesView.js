ecdace56a54d5a821c0156070abedad9a30fa8f8
kmschoep <kmschoep@IGSARMEWLTKMS.gs.doi.net>
2015-03-16 15:56:12 -0500

    a bit of code cleanup.

--- nwc/src/main/webapp/js/view/AquaticBiologySelectFeaturesView.js
+++ nwc/src/main/webapp/js/view/AquaticBiologySelectFeaturesView.js
@@ -12,14 +12,7 @@ NWC.view.AquaticBiologySelectFeaturesView = NWC.view.BaseView.extend({
             'click #selected-sites-button' : 'showSites',
             'click #allSelected' : 'selectAll'
         },
-        //render : function() {
-	//	NWC.view.BaseView.prototype.render.apply(this, arguments);
-        //        alert(JSON.stringify(this.model.getSites()));
-        //        var biodataSites = new NWC.collection.Features();
-        //        biodataSites.add(this.model.get('sites'));
-        //        return this;
-	//},
-        
+
         initialize : function() {
             this.context.biodataSites = this.model.get('sites');
             this.context.gages = this.model.get('gages');
@@ -28,18 +21,17 @@ NWC.view.AquaticBiologySelectFeaturesView = NWC.view.BaseView.extend({
             $('#gages-table-div').html({gages : this.model.get('gages')});
             $('#hucs-table-div').html({hucs : this.model.get('hucs')});
             NWC.view.BaseView.prototype.initialize.apply(this, arguments);
-            //this.listenTo(this.model, 'change', this.render);
         },
        
         checkboxChanged: function (evt) {
             
             $cb      = $(evt.target),
             name     = $cb.attr('name');
-            if (name != 'allSelected') {
+            if (name !== 'allSelected') {
                 if ($cb.is(':checked')) {
                     this.model.set({ 
                     'selected' : this.model.get('selected').concat(name)
-                });
+                    });
                 } else {
                     var selected = this.model.get('selected');
                     var index = selected.indexOf(name);
