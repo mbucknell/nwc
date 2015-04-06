3c02780a0a881b05c7758d02c3912b2f0adbde2c
Mark McDermond <mamcderm@usgs.gov>
2015-04-06 07:57:04 -0600

    watersmart-508; replacing test with a sinon fakeserver version

--- nwc/src/test/javascript/specs/DataDiscoveryView_spec.js
+++ nwc/src/test/javascript/specs/DataDiscoveryView_spec.js
@@ -1,8 +1,8 @@
-
-describe('Tests for DataDiscoveryView', function() {
+describe('Tests for DataDiscoveryView (sinon version)', function() {
 	var $testDiv;
 	var testView;
-
+	var server;
+	
 	beforeEach(function() {
 
 		$('body').append('<div id="test-div"></div>');
@@ -17,47 +17,47 @@ describe('Tests for DataDiscoveryView', function() {
 		CONFIG.endpoint.direct = {};
 		CONFIG.endpoint.direct.sciencebase = 'http://test';
 
-		spyOn(NWC.view.BaseView.prototype, 'initialize');
+		//do this to prevent intialize from running
+		spyOn(NWC.view.BaseView.prototype, 'initialize'); 
+		
+		server = sinon.fakeServer.create();
 
 	});
 
 	afterEach(function() {
 		$testDiv.remove();
+		server.restore();
 	});
 
 	it('Expects the view\'s constructor to call BaseView initialize', function() {
-		spyOn($, "ajax");
 		testView = new NWC.view.DataDiscoveryView();
 		expect(NWC.view.BaseView.prototype.initialize).toHaveBeenCalled();
 	});
 
 	it('Expects getProjectData to use the correct url for ajax call', function() {
-		spyOn($, "ajax");
 		testView = new NWC.view.DataDiscoveryView();
-		expect($.ajax.mostRecentCall.args[0]["url"]).toEqual(CONFIG.endpoint.direct.sciencebase +
-				'/catalog/items?facetTermLevelLimit=false&q=&community=National+Water+Census&filter0=browseCategory%3DProject&max=100&format=json');
+		expect(server.requests.last().url).toMatch('http://test');
 	});
 
-	it('Expects getProjectData to call getTemplate on successful ajax response', function() {
-	    spyOn($, "ajax").andCallFake(function(e) {
-	    	e.success({});
-	    });
-
+	it('Expects getProjectData to make an ajax call', function() {
+		var requestCount = server.requests.length;
 		var getTemplateSpy = jasmine.createSpy('getTemplateSpy')
 		var templateSpy = jasmine.createSpy('templateSpy');
 		NWC.templates = {
 			getTemplate : getTemplateSpy.andReturn(templateSpy)
 		};
-		testView = new NWC.view.DataDiscoveryView();
-		expect(NWC.templates.getTemplate).toHaveBeenCalled();
+		testView = new NWC.view.DataDiscoveryView();		
+		expect(server.requests.length).toBe(requestCount + 1);
 	});
 
 	it('Expects getProjectData to receive a error for no ajax response', function() {
-	    spyOn($, "ajax").andCallFake(function(e) {
-	    	e.error({});
-	    });
 		spyOn(window, 'alert');
-		testView = new NWC.view.DataDiscoveryView();
+		testView = new NWC.view.DataDiscoveryView();		
+		server.requests[0].respond(
+			400,
+			{ "Content-Type": "application/json" },
+			JSON.stringify([{}])
+		);
 		expect(alert).toHaveBeenCalled();
 	});
 
@@ -67,19 +67,13 @@ describe('Tests for DataDiscoveryView', function() {
 	});
 
 	it('Expects showProjectDetail to use the correct url for ajax call', function() {
-		spyOn($, "ajax");
 		testView = new NWC.view.DataDiscoveryView();
 		var event = {currentTarget : '#show-project-detail-button'}
 		testView.showProjectDetail(event);
-		expect($.ajax.mostRecentCall.args[0]["url"]).toEqual(CONFIG.endpoint.direct.sciencebase +
-				'/catalog/item/12345?format=json');
+		expect(server.requests.last().url).toMatch('http://test');
 	});
 
-	it('Expects showProjectDetail to call getTemplate on successful ajax response', function() {
-	    spyOn($, "ajax").andCallFake(function(e) {
-	    	e.success({});
-	    });
-
+	it('Expects showProjectDetail to make ajax call', function() {
 		var getTemplateSpy = jasmine.createSpy('getTemplateSpy')
 		var templateSpy = jasmine.createSpy('templateSpy');
 		NWC.templates = {
@@ -87,18 +81,22 @@ describe('Tests for DataDiscoveryView', function() {
 		};
 		testView = new NWC.view.DataDiscoveryView();
 		var event = {currentTarget : '#show-project-detail-button'}
+		var requestCount = server.requests.length;
 		testView.showProjectDetail(event);
-		expect(NWC.templates.getTemplate).toHaveBeenCalled();
+		expect(server.requests.length).toBe(requestCount + 1);
 	});
 
 	it('Expects showProjectDetail to receive a error for no ajax response', function() {
-	    spyOn($, "ajax").andCallFake(function(e) {
-	    	e.error({});
-	    });
 		spyOn(window, 'alert');
 		testView = new NWC.view.DataDiscoveryView();
-		var event = {currentTarget : '#show-project-detail-button'}
+		var event = {currentTarget : '#show-project-detail-button'};
 		testView.showProjectDetail(event);
+		server.requests[0].respond(
+				400,
+				{ "Content-Type": "application/json" },
+				JSON.stringify([{}])
+			);
 		expect(alert).toHaveBeenCalled();
 	});
+	
 });
