75b184f9db28a7ff8aec33764fe0cdebc99c0270
Mark McDermond <mamcderm@usgs.gov>
2015-06-04 10:05:07 -0600

    watersmart-428; add metadata catalog data catalog to data discovery

--- nwc/src/main/webapp/js/view/DataDiscoveryView.js
+++ nwc/src/main/webapp/js/view/DataDiscoveryView.js
@@ -6,7 +6,9 @@ NWC.view.DataDiscoveryView = NWC.view.BaseView.extend({
 	templateName : 'dataDiscovery',
 
 	events: {
-		'click #show-project-detail-button': "showProjectDetail",
+		'click #show-detail-button': "showDetail",
+		'click #show-projects-button': "showProjects",
+		'click #show-data-button': "showData",
 	},
 
 	initialize : function() {
@@ -14,19 +16,19 @@ NWC.view.DataDiscoveryView = NWC.view.BaseView.extend({
 		// call superclass initialize to do default initialize
 		// (includes render)
 		NWC.view.BaseView.prototype.initialize.apply(this, arguments);			
-
-		this.getProjectData();
+		$("#show-projects-button").prop('disabled', true);
+		this.getProjects();
 	},
 
 	/**
 	 * This makes a Web service call to get project list data for display
 	 */
-	getProjectData: function() {
+	getProjects: function() {
 		$.ajax({
 			url : CONFIG.endpoint.direct.sciencebase + '/catalog/items?facetTermLevelLimit=false&q=&community=National+Water+Census&filter0=browseCategory%3DProject&max=100&format=json',
 			dataType : "json",
 			success: function(data) {
-				$('#project-list-div').html(NWC.templates.getTemplate('projectList')({projectList : data.items}));
+				$('#list-div').html(NWC.templates.getTemplate('dataDiscoveryList')({list : data.items}));
 			},
 			error : function() {
 				//@todo - setup app level error handling
@@ -39,17 +41,17 @@ NWC.view.DataDiscoveryView = NWC.view.BaseView.extend({
 	/**
 	 * This makes a Web service call to get project detail data for display
 	 */
-	showProjectDetail: function(ev) {
-		var projectId = $(ev.currentTarget).data('id');
-		if ($('#' + projectId).is(':hidden')) {
+	showDetail: function(ev) {
+		var id = $(ev.currentTarget).data('id');
+		if ($('#' + id).is(':hidden')) {
 			$(ev.currentTarget).html('-');
 			$(ev.currentTarget).prop('title', 'Hide details');
 			$.ajax({
-				url : CONFIG.endpoint.direct.sciencebase + '/catalog/item/' + projectId + '?format=json',
+				url : CONFIG.endpoint.direct.sciencebase + '/catalog/item/' + id + '?format=json',
 				dataType : "json",
 				success: function(data) {
-					$('#' + projectId).html(NWC.templates.getTemplate('projectDetail')({projectDetail : data}));
-					$('#' + projectId).show();
+					$('#' + id).html(NWC.templates.getTemplate('dataDiscoveryDetail')({detail : data}));
+					$('#' + id).show();
 				},
 				error : function() {
 					//@todo - setup app level error handling
@@ -61,8 +63,73 @@ NWC.view.DataDiscoveryView = NWC.view.BaseView.extend({
 		else {
 			$(ev.currentTarget).html('+');
 			$(ev.currentTarget).prop('title', 'Show details');
-			$('#' + projectId).hide();
+			$('#' + id).hide();
 		}
+	},
+
+	/**
+	 * This makes a Web service call to get data for display
+	 */
+	getData: function() {
+		$.ajax({
+			url : CONFIG.endpoint.direct.sciencebase + '/catalog/items?facetTermLevelLimit=false&q=&community=National+Water+Census&filter0=browseCategory%3DData&max=100&format=json',
+			dataType : "json",
+			success: function(data) {
+				$('#list-div').html(NWC.templates.getTemplate('dataDiscoveryList')({list : data.items}));
+			},
+			error : function() {
+				//@todo - setup app level error handling
+				var errorMessage = 'error retrieving data list';
+				alert(errorMessage);
+			}
+		});
+	},
+
+	/**
+	 * This makes a Web service call to get project detail data for display
+	 */
+	showDataDetail: function(ev) {
+		var id = $(ev.currentTarget).data('id');
+		if ($('#' + id).is(':hidden')) {
+			$(ev.currentTarget).html('-');
+			$(ev.currentTarget).prop('title', 'Hide details');
+			$.ajax({
+				url : CONFIG.endpoint.direct.sciencebase + '/catalog/item/' + id + '?format=json',
+				dataType : "json",
+				success: function(data) {
+					$('#' + id).html(NWC.templates.getTemplate('dataDiscoveryDetail')({detail : data}));
+					$('#' + id).show();
+				},
+				error : function() {
+					//@todo - setup app level error handling
+					var errorMessage = 'error retrieving data detail';
+					alert(errorMessage);
+				}
+			});
+		}
+		else {
+			$(ev.currentTarget).html('+');
+			$(ev.currentTarget).prop('title', 'Show details');
+			$('#' + id).hide();
+		}
+	},
+
+	/**
+	 * This toggles the view to display projects
+	 */
+	showProjects: function() {
+		$('#show-data-button').prop('disabled', false);
+		$("#show-projects-button").prop('disabled', true);
+		this.getProjects();
+	},
+
+	/**
+	 * This toggles the view to display data
+	 */
+	showData: function() {
+		$('#show-data-button').prop('disabled', true);
+		$("#show-projects-button").prop('disabled', false);
+		this.getData();
 	}
 
 });
