f054fc25625a96d97e5d1a490e8d86da55eb1b8b
kmschoep <kmschoep@IGSARMEWLTKMS.gs.doi.net>
2015-06-26 15:09:10 -0500

    refactored csv download to collection model, overall indent cleanup.

--- nwc/src/main/webapp/js/view/AquaticBiologySelectFeaturesView.js
+++ nwc/src/main/webapp/js/view/AquaticBiologySelectFeaturesView.js
@@ -6,194 +6,142 @@ NWC.view = NWC.view || {};
 
 NWC.view.AquaticBiologySelectFeaturesView = NWC.view.BaseView.extend({
 	templateName : 'aquaticBiologySelectFeatures',  
-        context : {
-        },
-        
-        events: {
-			'change input[type=checkbox]': 'checkboxChanged',
-			'click #selected-sites-button' : 'showSites',
-			'click #allSelected' : 'selectAll',
-			'click #biodata-form-button' : 'sitesDoc',
-			'click #hucs-table-div tr' : 'onHucSelect',
-			'click #gages-table-div tr' : 'onGageSelect',
-			'click #download-pairs-button' : 'downloadPairs'
-		},
 
-        initialize : function() {
-			this.context.biodataSites = this.model.get('sites');
-			this.context.gages = this.model.get('gages');
-			this.context.hucs = this.model.get('hucs');
-			this.collection = new NWC.model.PairCollection();
-			// Header information for the pair list download file
-			this.metadata = {
-					seriesLabels: [
-						{seriesName: 'SiteNumber'},
-						{seriesName: 'SiteName'},
-						{seriesName: 'SiteDrainageAreaSqMi'},
-						{seriesName: 'GageID'},
-						{seriesName: 'GageName'},
-						{seriesName: 'GageDrainageAreaSqKM'},
-						{seriesName: 'Comment'}
-					],
-					downloadHeader: "Aquatic Biology Site-Gage pair selections"
-				},
-			this.pairViews = [];
-			NWC.view.BaseView.prototype.initialize.apply(this, arguments);
-			this.listenTo(this.collection, 'add', this.displayPairList);
-			this._displayMap();
-        },
+	context : {
+	},
+
+	events: {
+		'change input[type=checkbox]': 'checkboxChanged',
+		'click #selected-sites-button' : 'showSites',
+		'click #allSelected' : 'selectAll',
+		'click #biodata-form-button' : 'sitesDoc',
+		'click #hucs-table-div tr' : 'onHucSelect',
+		'click #gages-table-div tr' : 'onGageSelect',
+		'click #download-pairs-button' : 'downloadPairs'
+	},
+
+	initialize : function() {
+		this.context.biodataSites = this.model.get('sites');
+		this.context.gages = this.model.get('gages');
+		this.context.hucs = this.model.get('hucs');
+		this.collection = new NWC.model.PairCollection();
+		this.pairViews = [];
+		NWC.view.BaseView.prototype.initialize.apply(this, arguments);
+		this.listenTo(this.collection, 'add', this.displayPairList);
+		this._displayMap();
+	},
        
-        checkboxChanged: function (evt) {
-			var $cb = $(evt.target),
-			name = $cb.attr('name');
-			var disable = !($('#sites-table-div input').is(':checked'));
-			$('#biodata-form-button').prop('disabled', disable);
-			if (name !== 'allSelected') {
-				var selected = this.model.get('selected');
-				if ($cb.is(':checked')) {
-					$('#' + name + '-btn').prop('disabled', false);
-					selected.add(name);
-				} else {
-					$('#' + name + '-btn').prop('disabled', true);
-					selected.remove(name);
-				};
-			}; 
-		},
-        
-        selectAll : function(evt){
-			var $cb      = $(evt.target);
-			var checkAll = ($cb.prop('checked'));
-			$('.sites-table td input[type="checkbox"]').each(function() {
-				$(this).prop('checked', checkAll).change();
-			});
-		},
-        // send selected sites to Bioshare and pre-populate the sites filter with those sites
-        sitesDoc : function () {
-			var bioDataSiteSelectionDoc;
-			var preselectBioDataSites = function (siteIds) {
-				var doc = bioDataSiteSelectionDoc;
-				var siteNumbersElt = $(doc).find('siteNumbers').empty()[0];
-				siteIds.each(function (siteId) {
-					var child = doc.createElement('siteNumber');
-					child.textContent = siteId;
-					siteNumbersElt.appendChild(child);
-				});
-				//serialize xml document
-				var xmlString = "";
-				try {
-					xmlString = (new XMLSerializer()).serializeToString(doc);
-				} catch(e) {}
-
-				//Give IE a shot
-				if (xmlString.length <= 0 && window.ActiveXObject) {
-					xmlString = doc.xml;
-				}
-				$("[name='currentQuery']").val(xmlString);
-				$('#bioData_form').submit();
-                };
-			var siteIds = this.model.get('selected');
-				
-			if (bioDataSiteSelectionDoc) {
-				preselectBioDataSites(siteIds);
+	checkboxChanged: function (evt) {
+		var $cb = $(evt.target),
+		name = $cb.attr('name');
+		var disable = !($('#sites-table-div input').is(':checked'));
+		$('#biodata-form-button').prop('disabled', disable);
+		if (name !== 'allSelected') {
+			var selected = this.model.get('selected');
+			if ($cb.is(':checked')) {
+				$('#' + name + '-btn').prop('disabled', false);
+				selected.add(name);
 			} else {
-				//retrieve document from server
-				$.ajax({
-					url: 'templates/xml/BioDataSiteSelection.xml',
-					success : function (response, status, jqXHR) {
-						bioDataSiteSelectionDoc = response;
-						preselectBioDataSites(siteIds);
-					},
-					error : function (response, status, jqXHR) {
-						alert("Error Retrieving BioData query skeleton");
-					},
-					context : this
-				});
-			}
-		},
-
-		onHucSelect : function(e){
-			e.preventDefault();
-			$cb = $(e.currentTarget);
-			hucID = $cb.attr('id');
-			this.router.navigate('/streamflow-stats/huc/' + hucID, {trigger : true});
-        } ,
-		
-        onGageSelect : function(e){
-			e.preventDefault();
-			$cb = $(e.currentTarget);
-			gageID = $cb.attr('id');
-			this.router.navigate('/streamflow-stats/gage/' + gageID, {trigger : true});
-        },
-	    
-		displayPairList : function (pair) {
-			var pairView = new NWC.view.AquaticBiologyPairView({
-				model: pair
-			});
-			this.$("#pair-list").append(pairView.render().el);
-			// push created views to an array so that they can be removed later
-			this.pairViews.push(pairView);
-		},
-	
-		_displayMap : function() {
-			this.biodataGageMapView = new NWC.view.BiodataGageMapView({
-				mapDiv : 'biodata-gage-selection-map',
-				biodataFeature : this.context.biodataSites,
-				gageFeature : this.context.gages,
-				router : this.router,
-				el : $('#biodata-gage-selection-div'),
-				collection : this.collection
-			});
-        },
-		// loop through the collection of pairs and returns array of rows of pairs
-		getPairData : function() {
-			var pairData = [];
-			pairData = this.collection.map(function(model){
-				var rows;
-				rows	= [model.get("site").SiteNumber,
-				model.get("site").SiteName,
-				model.get("site").DrainageAr,
-				model.get("gage").STAID,
-				model.get("gage").STANAME,
-				model.get("gage").DRAIN_SQKM,
-				model.get("comment")];
-			return rows;
-			});
-			return pairData;
-		},
-		
-		toCSV: function() {
-			var data = this.getPairData();
-			var createSeriesLabel = function (metadata) {
-				var label = metadata.seriesName;
-				return label;
+				$('#' + name + '-btn').prop('disabled', true);
+				selected.remove(name);
 			};
-			var csvHeader = "";
-			if (this.metadata.downloadHeader && this.metadata.downloadHeader.length !== 0) {
-				this.metadata.downloadHeader.lines(function(line) {
-					csvHeader += "\"# " + line + "\"\r\n";
-				});
+		}; 
+	},
+        
+	selectAll : function(evt){
+		var $cb      = $(evt.target);
+		var checkAll = ($cb.prop('checked'));
+		$('.sites-table td input[type="checkbox"]').each(function() {
+			$(this).prop('checked', checkAll).change();
+		});
+	},
+	// send selected sites to Bioshare and pre-populate the sites filter with those sites
+	sitesDoc : function () {
+		var bioDataSiteSelectionDoc;
+		var preselectBioDataSites = function (siteIds) {
+			var doc = bioDataSiteSelectionDoc;
+			var siteNumbersElt = $(doc).find('siteNumbers').empty()[0];
+			siteIds.each(function (siteId) {
+				var child = doc.createElement('siteNumber');
+				child.textContent = siteId;
+				siteNumbersElt.appendChild(child);
+			});
+			//serialize xml document
+			var xmlString = "";
+			try {
+				xmlString = (new XMLSerializer()).serializeToString(doc);
+			} catch(e) {}
+				//Give IE a shot
+			if (xmlString.length <= 0 && window.ActiveXObject) {
+				xmlString = doc.xml;
 			}
-			csvHeader += this.metadata.seriesLabels.map(function(label) {
-				return createSeriesLabel(label);
-			}).join(",") + "\r\n";
-			var csvValues = "";
-			data.each(function(row) {
-				// Add enclosing quotes to account for commas in the site and/or gage names
-				csvValues += "\"" + row.join("\",\"") + "\"\r\n";
+			$("[name='currentQuery']").val(xmlString);
+			$('#bioData_form').submit();
+			};
+		var siteIds = this.model.get('selected');
+	
+		if (bioDataSiteSelectionDoc) {
+			preselectBioDataSites(siteIds);
+		} else {
+			//retrieve document from server
+			$.ajax({
+				url: 'templates/xml/BioDataSiteSelection.xml',
+				success : function (response, status, jqXHR) {
+					bioDataSiteSelectionDoc = response;
+					preselectBioDataSites(siteIds);
+				},
+				error : function (response, status, jqXHR) {
+					alert("Error Retrieving BioData query skeleton");
+				},
+				context : this
 			});
-			return csvHeader + csvValues;
-		},
-		
-		downloadPairs : function() {
-			var blob = new Blob([this.toCSV()], {type:'text/csv'});
-			saveAs(blob, 'AquaticBiologyNWISGagePairs.csv');
-		},
-		
-		remove : function() {
-			this.pairViews.each(function(pv){
-				pv.remove();
-			});
-			NWC.view.BaseView.prototype.remove.apply(this, arguments);
 		}
+	},
+
+	onHucSelect : function(e){
+		e.preventDefault();
+		$cb = $(e.currentTarget);
+		hucID = $cb.attr('id');
+		this.router.navigate('/streamflow-stats/huc/' + hucID, {trigger : true});
+	},
+	
+	onGageSelect : function(e){
+		e.preventDefault();
+		$cb = $(e.currentTarget);
+		gageID = $cb.attr('id');
+		this.router.navigate('/streamflow-stats/gage/' + gageID, {trigger : true});
+	},
+	   
+	displayPairList : function (pair) {
+		var pairView = new NWC.view.AquaticBiologyPairView({
+			model: pair
+		});
+		this.$("#pair-list").append(pairView.render().el);
+		// push created views to an array so that they can be removed later
+		this.pairViews.push(pairView);
+	},
+	
+	_displayMap : function() {
+		this.biodataGageMapView = new NWC.view.BiodataGageMapView({
+			mapDiv : 'biodata-gage-selection-map',
+			biodataFeature : this.context.biodataSites,
+			gageFeature : this.context.gages,
+			router : this.router,
+			el : $('#biodata-gage-selection-div'),
+			collection : this.collection
+		});
+	},
+		
+	downloadPairs : function() {
+		var blob = new Blob([this.collection.toCSV()], {type:'text/csv'});
+		saveAs(blob, 'AquaticBiologyNWISGagePairs.csv');
+	},
+		
+	remove : function() {
+		this.pairViews.each(function(pv){
+			pv.remove();
+		});
+		NWC.view.BaseView.prototype.remove.apply(this, arguments);
+	}
 		
 });
