b871b34aa3f16be634333452bdbc1bca465e51af
Carl Schroedl <cschroedl@usgs.gov>
2015-08-28 17:41:00 -0500

    Proof of concept.

--- nwc/src/main/java/gov/usgs/cida/nwc/servlets/SkeletonPageServlet.java
+++ nwc/src/main/java/gov/usgs/cida/nwc/servlets/SkeletonPageServlet.java
@@ -4,14 +4,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import gov.usgs.cida.simplehash.SimpleHash;
+import java.io.IOException;
 
 public class SkeletonPageServlet extends HttpServlet{
 
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response){
-		String urlWithoutContextPath = getUrlWithoutContextPath(request);
-		
-		
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException{
+//		String urlWithoutContextPath = getUrlWithoutContextPath(request);
+		response.getWriter().append("WARKS").close();
 	}
 	
 	/**
